6c2ecfbb91cf519204e23d0abb248f992aeda75a
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-30 23:42:44 +0300

    getLocaleTime func added.

--- src/components/people-card.js
+++ src/components/people-card.js
@@ -1,5 +1,9 @@
 import { h } from 'hyperapp';
 
-const PeopleCard = ({ people }) => h("div", { class: "people-card" }, h("div", { class: "people_card__title"}, people.name));
+const getLocaleTime = ((timeZone) => {
+    let localeTime = new Date().toLocaleString("en-US", {timeZone: timeZone});
+    localeTime = new Date(localeTime);
+    return localeTime;
+});
 
 export default PeopleCard;
