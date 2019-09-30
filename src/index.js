b987145b6ac40a2737a12f279baddf0b23579237
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-09-30 21:41:58 +0300

    İnitial commit.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { h, app } from "hyperapp";
+
+app({
+    init: 0,
+    view: state =>
+        h("div", {}, [
+            h("h1", {}, state),
+            h("button", { onClick: state => state - 1 }, "-"),
+            h("button", { onClick: state => state + 1 }, "+")
+        ]),
+    node: document.getElementById("app")
+});
